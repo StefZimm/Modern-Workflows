431d5250d41bc532acd7d8b7b1d8407ee1243424
Stefan Zimmermann <szimmermann@diw.de>
2020-07-12 11:08:50 +0000

    create plots for report

--- assignment_3/scripts/Analytical_Notbook_Stefan_Zimmermann.R
+++ assignment_3/scripts/Analytical_Notbook_Stefan_Zimmermann.R
@@ -1,6 +1,6 @@
-# install.packages("sparklyr")
+install.packages("sparklyr")
 library(sparklyr)
-# spark_install()
+spark_install()
 
 rmarkdown::render('assignment_3/scripts/Analytical_Notebook_Stefan_Zimmermann.Rmd', 
                   output_file = '../output/Analytical_Notebook_Stefan_Zimmermann.pdf')
